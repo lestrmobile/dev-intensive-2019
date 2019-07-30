2fcdf9821b3de55e870a5bacbd4e9dfdd81b8ccd
lestrmobile <lestrmobile@gmail.com>
2019-07-30 14:13:21 +0300

    hometask 2 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/Extensions/User.kt
@@ -0,0 +1,22 @@
+package ru.skillbranch.devintensive.Extensions
+
+import ru.skillbranch.devintensive.models.User
+import ru.skillbranch.devintensive.models.UserView
+import ru.skillbranch.devintensive.utils.Utils
+
+fun User.toUserView() : UserView{
+    val nickName = Utils.transliteration("$firstName $lastName")
+    val initials = Utils.toinitials(firstName, lastName)
+    val status = if (lastVisit == null) "Ещё ни разу не был" else if (isOnline) "online" else "Последний раз был ${lastVisit.humanizeDiff()}"
+
+    return  UserView(
+        id,
+        fullName = "$firstName $lastName",
+        avatar = avatar,
+        initials = initials ,
+        nickName = nickName,
+        status = status)
+
+}
+
+
---
+++ app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
@@ -0,0 +1,17 @@
+package ru.skillbranch.devintensive.models
+
+import ru.skillbranch.devintensive.Extensions.humanizeDiff
+import java.util.*
+
+class ImageMessage (
+    id:String,
+    Date:User?,
+    chat:Chat,
+    isIncoming : Boolean = false,
+    date: Date = Date(),
+    var image:String?
+): BaseMessage(id, Date,chat,isIncoming,date) {
+   override fun formatMessage(): String = "id:$id ${from?.firstName}"+
+            " ${if(isIncoming) "получил" else "отправил"} изображение \"$image\" ${date.humanizeDiff()}"
+
+}
