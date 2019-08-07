ee18155b2e118d414640ebbb5480ef9bd27329b1
lestrmobile <lestrmobile@gmail.com>
2019-08-07 14:25:47 +0300

    hometask 3 complete

--- app/src/main/java/ru/skillbranch/devintensive/Extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/Extensions/Activity.kt
@@ -1,7 +1,6 @@
 package ru.skillbranch.devintensive.Extensions
 
 import android.app.Activity
-import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
