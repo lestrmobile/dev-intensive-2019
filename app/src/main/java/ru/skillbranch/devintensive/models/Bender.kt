ece13da0e3e91656012cd651b72039157cccec0e
lestrmobile <lestrmobile@gmail.com>
2019-08-08 17:34:57 +0300

    hometask 3 complete

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.devintensive.models
 
+import androidx.core.text.isDigitsOnly
+
 class Bender(var status:Status = Status.NORMAL, var question:Question = Question.NAME) {
 
     var cnt:Int = 0
@@ -15,13 +17,35 @@ class Bender(var status:Status = Status.NORMAL, var question:Question = Question
     }
 
     fun listenAnswer(answer:String) : Pair<String,Triple<Int,Int,Int>>{
-        /*
-        if (cnt == 3){
-            resetBender()
-            return  "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+        val regex = Regex(pattern = "0|1|2|3|4|5|6|7|8|9")
+        var ans:String
+
+        if (question != Question.IDLE){
+            if (question == Question.NAME) {
+                if (answer[0].isLowerCase()) {
+                    return "Имя должно начинаться с заглавной буквы\n${question.question}" to status.color
+                }
+            } else if (question == Question.PROFESSION) {
+                if (answer[0].isUpperCase()) {
+                    return "Профессия должна начинаться со строчной буквы\n${question.question}" to status.color
+                }
+            } else if (question == Question.MATERIAL) {
+                if (answer.contains(regex)) {
+                    return "Материал не должен содержать цифр\n${question.question}" to status.color
+                }
+            } else if (question == Question.BDAY) {
+                if (answer.isDigitsOnly().not()) {
+                    return "Год моего рождения должен содержать только цифры\n${question.question}" to status.color
+                }
+            } else if (question == Question.SERIAL) {
+                if ((answer.isDigitsOnly().not()) || ((answer.length == 7).not())) {
+                    return "Серийный номер содержит только цифры, и их 7\n${question.question}" to status.color
+                }
+            }
         }
-        */
-        if (question.answers.contains(answer)) {
+        ans = answer.toLowerCase()
+
+        if (question.answers.contains(ans)) {
             cnt=0
             question = question.nextQuestion()
             return "Отлично - ты справился\n${question.question}" to status.color
