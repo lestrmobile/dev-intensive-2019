2a6f0fa4a5dcd080d9942706ed2e6c18f95f6540
lestrmobile <lestrmobile@gmail.com>
2019-08-09 16:10:22 +0300

    hometask 3 complete

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.skillbranch.devintensive
 
 
+import android.app.Activity
 import ru.skillbranch.devintensive.extensions.hideKeyboard
 import android.graphics.Color
 import android.graphics.PorterDuff
@@ -13,6 +14,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.extensions.isKeyboardOpen
 import ru.skillbranch.devintensive.models.Bender
 
 class MainActivity : AppCompatActivity(),View.OnClickListener {
@@ -94,6 +96,7 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         outState?.putString("STATUS", benderObj.status.name)
         outState?.putString("QUESTION", benderObj.question.name)
         Log.d("M_MainActivity","onSaveInstanceState ${benderObj.status.name} ${benderObj.question.name}")
+        isKeyboardOpen()
     }
 
     override fun onClick(v: View?) {
@@ -101,6 +104,7 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
             if (!messageEt.text.isEmpty()) {
                 val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString())
                 messageEt.setText("")
+
                 val (r, g, b) = color
                 benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
                 textTxt.text = phrase
--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,8 +1,15 @@
 package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
+import android.graphics.Rect
+import android.util.Log
+import android.util.TypedValue
 import android.view.View
+import android.view.ViewTreeObserver
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import ru.skillbranch.devintensive.R
 
 fun Activity.hideKeyboard() {
     val currentFocus:View? = getCurrentFocus()
@@ -10,3 +17,47 @@ fun Activity.hideKeyboard() {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 }
+//
+//fun Activity.isKeyboardOpen():Boolean{
+//
+//    val ActivityRootView: View = findViewById(R.id.activityRoot)
+//    ActivityRootView.getViewTreeObserver().addOnGlobalLayoutListener(ViewTreeObserver.OnGlobalLayoutListener());
+//
+//
+//    override fun OnGlobalLayoutListener(){
+//
+//
+//
+//
+//
+//    return false
+//}
+//
+//fun Activity.isKeyboardClosed():Boolean{
+//    return true
+//}
+
+
+fun Activity.isKeyboardOpen(): Boolean {
+    val permissibleError = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50F, resources.displayMetrics).toInt()
+    val rootView = findViewById<View>(android.R.id.content)
+    var rect = Rect()
+    rootView.getWindowVisibleDisplayFrame(rect)
+//    Log.d("M_Activity","${rootView.height}/${rect.height()}")
+    val difference = rootView.height - rect.height()
+    return (difference > permissibleError)
+}
+
+fun Activity.isKeyboardClosed():Boolean{
+    val permissibleError = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50F, resources.displayMetrics).toInt()
+    val rootView = findViewById<View>(android.R.id.content)
+    var rect = Rect()
+    rootView.getWindowVisibleDisplayFrame(rect)
+//    Log.d("M_Activity","${rootView.height}/${rect.height()}")
+    val difference = rootView.height - rect.height()
+    return (difference < permissibleError)
+}
+
+/*
+ Реализуй extension для проверки, открыта или нет Software Keyboard с применением метода rootView.getWindowVisibleDisplayFrame(Rect())
+*/
