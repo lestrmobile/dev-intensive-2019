5a8e13a7be02df91327da891a1fe4de57a50e270
lestrmobile <lestrmobile@gmail.com>
2019-08-12 15:38:44 +0300

    hometask 2 complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -38,20 +38,26 @@ fun Date.humanizeDiff(date : Date = Date()): String {
        in 46L * SECOND..75 * SECOND ->  "минуту назад"
        in 75L * SECOND..45L * MINUTE -> "${dlt / MINUTE} минут назад"
        in 45L * MINUTE..75L * MINUTE  -> "час назад"
-       in 75L * MINUTE..22L* HOUR ->  "${dlt / HOUR} часа назад"
+       in 75L * MINUTE..4L* HOUR ->  "${dlt / HOUR} часа назад"
+       in 5L * HOUR..22L* HOUR ->  "${dlt / HOUR} часов назад"
        in 22L * HOUR..26L * HOUR -> "день назад"
-       in 26L * HOUR..360 * DAY -> "${dlt / DAY} дней назад"
+       in 26L * HOUR..4 * DAY -> "${dlt / DAY} дня назад"
+       in 5L * DAY..360 * DAY -> "${dlt / DAY} дней назад"
        in 360 * DAY..36000 * DAY ->  "более года назад"
 
        in -0L * SECOND downTo -1L * SECOND -> "только что"
        in -1L * SECOND downTo -45L * SECOND -> "через несколько секунд"
        in -46L * SECOND downTo -75L * SECOND ->  "через минуту"
-
        in -75L * SECOND downTo -45L * MINUTE -> "через ${dlt / MINUTE*-1} минут "
        in -45L * MINUTE downTo -75L * MINUTE  -> "через час"
-       in -75L * MINUTE downTo -22L* HOUR -> "через ${dlt / HOUR*-1} часов"
+       in -75L * MINUTE downTo -4L* HOUR -> "через ${dlt / HOUR*-1} часа"
+       in -5L * DAY downTo -22L* HOUR -> "через ${dlt / HOUR*-1} часов"
+
        in -22L * HOUR downTo -26L * HOUR -> "через день"
-       in -26L * HOUR downTo -360L * DAY -> "через ${dlt / DAY*-1} дней"
+       in -26L * HOUR downTo -4L * DAY -> "через ${dlt / DAY*-1} дня"
+
+       in -5L * DAY downTo -360L * DAY -> "через ${dlt / DAY*-1} дней"
+
        in -360 * DAY downTo -36000L * DAY ->  "более чем через год"
        else -> "Хрен знает когда"
    }
