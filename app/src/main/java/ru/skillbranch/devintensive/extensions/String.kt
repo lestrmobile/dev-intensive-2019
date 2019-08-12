de95eefd72e3fe3496bd96ded31e4f7f07d116d7
lestrmobile <lestrmobile@gmail.com>
2019-08-12 19:08:12 +0300

    hometask 2 complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -11,3 +11,37 @@ fun String.truncate(len:Int = 16):String{
   r += "..."
   return r
 }
+
+fun String.stripHtml():String{
+  val s:String = this.toString()
+  var tg: Boolean = false
+  var br: Boolean =false
+  var r:String =""
+  for(i in 0..s.length-1) {
+    if (s[i].toString() == " "&&s[i+1].toString() == " ") {
+      r += ""
+      continue
+    }
+
+
+    if (s[i].toString() == "<") tg = true
+    else if (s[i].toString() == ">"){
+      tg = false
+      continue
+    }
+    if (tg) {
+      r += ""
+      continue
+    } else {
+      r += s[i]
+      continue
+    }
+
+
+
+
+
+  }
+
+  return r
+}
